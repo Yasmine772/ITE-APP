ce26384ae3a5d983d2a3fe08c0489ed372764ab0
yasmin Ebrahim <yasmineebrahim458@gmail.com>
2025-06-20 08:19:51 +0300

    Update UserServices.php

--- app/Services/UserServices.php
+++ app/Services/UserServices.php
@@ -44,7 +44,7 @@ class UserServices
         $message = 'User login successfully';
         return [
             'token'=>$token ,
-            git 'message'=> $message
+            'message'=> $message
         ];
     }
 
